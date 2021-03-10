18328d9aca4b2fb0acae9bb88f4577653d735359
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-10 05:10:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :authors, only: [:show, :index] do
-  resources :posts, only: [:show, :index, :new]
-end
-resources :posts
+    resources :posts, only: [:show, :index, :new, :edit]
+  end
+  resources :posts
 
 end
